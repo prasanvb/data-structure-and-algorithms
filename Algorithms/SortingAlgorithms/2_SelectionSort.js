9b39807486169ba1a73a69cffbecdcb783cdaae0
prasanv <prasan.venkatb@gmail.com>
2022-08-11 13:16:43 -0700

    updated some sorts

--- Algorithms/SortingAlgorithms/2_SelectionSort.js
+++ Algorithms/SortingAlgorithms/2_SelectionSort.js
@@ -7,15 +7,44 @@
   // If any other item in the array is smaller than first item then swap the values.
   // Exclude the sorted items and select the next item repeat the process
 
-  // steps
-  // 1. first for loop iterates from 0 to array length-1
-  // 2. second for loop iterates from 1 to array length and does the comparison and swapping 
+  // Steps
+  // 1. declare minIndex variable
+  // 2. create for-loop from i=0 to i < length-1 and set minIndex to i
+  // 3. create for-loop from j=i+1 to j < length
+  // 4. compare minIndex item value to j, if later is smaller then set minIndex to j
+  // 5. repeat step 4 on rest of the items in array
+  // 6. check if minIndex is still same as i, if not true swap the values on the respective indexes
 
 
-let arr = [12,4,9,3,70,1e+0,4.4,3.000,1.23456e+5];
+let arr = [12,4,9,3,70,1e+0,4.4,1.23456e+5,3.000];
+
+console.log("OriginalArray = " + arr);
 
 const selectionSort = (arr) => {
+  let minIndex;
 
+  for(let i=0; i<arr.length-1; i++){
+    // Important note: Initially we are assuming minIndex is first item in array
+    minIndex = i;
+
+    for(let j=i+1; j<arr.length; j++ ){
+      if(arr[minIndex] > arr[j]){
+        minIndex = j;
+      }
+    }
+
+    if(minIndex!==i){
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log("selectionSort = " + selectionSort(arr));
+
+const similarToSelectionSort = (arr) => {
   let iterator = 0;
   for(let i=0;  i<arr.length-1; i++){
     for(let j=i+1; j <arr.length; j++){
@@ -32,32 +61,4 @@ const selectionSort = (arr) => {
   return arr;
 }
 
-console.log(`[${selectionSort(arr)}]`);
-
-
-// Alternate old School Approach
-function selectionSort_alternate(array) {
-  let arr = array.slice();
-  let iterator = 0;
-  for (let i = 0; i < arr.length - 1; i++) {
-    let min = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      iterator++;
-      if (arr[j] < arr[min]) {
-        min = j;
-      }
-    }
-    if (min !== i) {
-      let temp = arr[i];
-      arr[i] = arr[min];
-      arr[min] = temp;
-    }
-  }
-  console.log({ iterator });
-  return arr;
-}
-
-console.log(`[${selectionSort_alternate(arr)}]`);
-
-
-console.log(`Original Array ${arr}`);
+// console.log(`[${similarToSelectionSort(arr)}]`);
--- Algorithms/SortingAlgorithms/3_Insertionsort.js
+++ Algorithms/SortingAlgorithms/3_Insertionsort.js
@@ -11,40 +11,40 @@
   // 2. second while loop does the comparison and switching
   // 3. after while loop, we increment the while loop variable
   
-  let arr = [12,4,9,3,70,1e+0,4.4,3.000,1.23456e+5];
+let arr = [12,4,9,3,70,1e+0,4.4,1.23456e+5,3.000];
 
-  console.log(`${arr}`);
+console.log("OriginalArray = " + arr);
+
+const insertionSort = (arr) => {
   
-  const insertionSort = (arr) => {
-    
-    for(let i=1; i<arr.length; i++){
-      let numberToBeInserted = arr[i];
-      
-      let j=i-1;
-      // console.log(j, arr[j]);
-      while(j >= 0 && (arr[j]>numberToBeInserted)){
-        // Note: Carefully observe how the assignment happens here
-        arr[j+1]=arr[j];
-        j--;
-      }
-      
-      // IMPORTANT NOTE:
-      // The value of j when we reach this line will have already been decremented by 1 by the while loop
-      // console.log(j, arr[j]);
-      arr[j+1] = numberToBeInserted;
-      console.log(`${arr}`);
+  for(let i=1; i<arr.length; i++){
+    let numberToBeInserted = arr[i];
+    let j=i-1;
+    // console.log(j, arr[j]);
+
+    while(j >= 0 && (arr[j]>numberToBeInserted)){
+      // Note: Carefully observe how the assignment happens here
+      arr[j+1]=arr[j];
+      j--;
     }
     
-    return arr;
+    // IMPORTANT NOTE:
+    // The value of j when we reach this line will have already been decremented by 1 by the while loop
+    // console.log(j, arr[j]);
+    arr[j+1] = numberToBeInserted;
   }
   
-  console.log("insertionSort = " + insertionSort(arr));
-    
+  return arr;
+}
+
+console.log("insertionSort = " + insertionSort(arr));
+  
 
   
-let myArray = [4, 2, 6, 5, 1, 3];
+// Insertion sort using for loop
 
-function insertionSort(array) {
+let myArray = [4, 2, 6, 5, 1, 3];
+function insertionSort_2(array) {
   let arr = array.slice();
   let temp;
   let iterator = 0;
@@ -59,9 +59,10 @@ function insertionSort(array) {
     }
     arr[j + 1] = temp;
   }
-  console.log({ iterator });
+  // console.log({ iterator });
   return arr;
 }
 
 
-console.log("insertionSort = " + insertionSort(myArray));
+console.log("insertionSort_2 = " + insertionSort_2(myArray));
+
