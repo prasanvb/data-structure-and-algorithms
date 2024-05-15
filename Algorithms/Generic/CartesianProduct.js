f664c82ad536e0e9b7e33351d7147954d98e2f92
prasanv <prasan.venkatb@gmail.com>
2024-05-15 13:03:06 -0700

    cartesian product

---
+++ Algorithms/Generic/CartesianProduct.js
@@ -0,0 +1,25 @@
+// Cartesian product
+// In mathematics, specifically set theory, the Cartesian product of two sets A and B, denoted AxB,
+// is the set of all ordered pairs (a, b) where a is in A and b is in B
+// const A = [1, 2]
+// const B = [3, 4]
+// AxB = [ [1, 3], [1, 4], [2, 3], [2, 4] ]
+
+
+
+
+const cartesianProduct = (arr1, arr2) => {
+    let tempArr = [];
+    
+    for(let i of arr1){
+      for(let j of arr2){
+        tempArr.push([i, j]);
+      }  
+    }
+    
+    return tempArr;
+  }
+  
+  const arr1 = [1, 2];
+  const arr2 = [3, 4, 5];
+  console.log(cartesianProduct(arr1, arr2));
