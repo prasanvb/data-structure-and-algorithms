67c8f82053a1f8ba0727898d0a6085c0ca76798b
prasanv <prasan.venkatb@gmail.com>
2024-05-14 21:53:45 -0700

    update sort

--- Algorithms/SortingAlgorithms/4_QuickSort.js
+++ Algorithms/SortingAlgorithms/4_QuickSort.js
@@ -1,25 +1,24 @@
 const quickSort = (arr) => {
-  if(arr.length < 2){
+  if (arr.length < 2) {
     return arr;
   }
-  
+
   // console.log({arr});
 
-  let pivot=arr[arr.length-1];
-  let leftArr=[];
-  let rightArr=[];
-  
-  for(let i=0; i < arr.length-1; i++){
-    if(arr[i] < pivot){
+  let pivot = arr[arr.length - 1];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
       leftArr.push(arr[i]);
-    } else{
+    } else {
       rightArr.push(arr[i]);
     }
   }
   // console.log({leftArr, pivot, rightArr});
-  
-  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
-  
-}
 
-console.log(quickSort([4,3,-8,7,-1, 87, 0,]));
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+};
+
+console.log(quickSort([4, 3, -8, 7, -1, 87, 0]));
---
+++ Algorithms/SortingAlgorithms/5_MergeSort.js
@@ -0,0 +1,33 @@
+// Merge Sort
+// Divide the array into sub arrays, each containing only one element (An array with one element is considered sorted)
+// Repeatedly merge the sub arrays to produce new sorted sub arrays until there is only one sub array remaining. That will be the sorted array.
+
+const mergeSort = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  let mid = Math.floor(arr.length / 2);
+  let leftArr = arr.slice(0, mid);
+  let rightArr = arr.slice(mid);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+};
+
+const merge = (leftArr, rightArr) => {
+  let sortedArr = [];
+
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+
+  // console.log({sortedArr, leftArr,rightArr});
+
+  return [...sortedArr, ...leftArr, ...rightArr];
+};
+
+console.log(mergeSort([4, 3, -8, 7, -1, 87, 0]));
