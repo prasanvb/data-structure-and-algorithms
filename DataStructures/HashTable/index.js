40b6ea2900a33827cac526de460f492ff8b440bf
prasanv <prasan.venkatb@gmail.com>
2024-05-16 22:32:41 -0700

    update hash table

--- DataStructures/HashTable/index.js
+++ DataStructures/HashTable/index.js
@@ -1,77 +1,94 @@
-function logger(value) {
-  console.dir(value, { depth: null });
-  console.log("-----------------------");
-}
+// Hash Table 
+// Set to store a key-value pair
+// Get to retrieve a value given its key 
+// Remove to delete a key value pair
+// Hashing function to convert a string key to a numeric index
+
+// Sample Hash Table
+// [[ [ 'age', 35 ] ], [ [ 'name', 'prasan' ], [ 'nema', 'venkat' ] ]]
+// { index: 1, bucket: [ [ 'age', 35 ] ] }
+// { index: 17, bucket: [ [ 'name', 'prasan' ], [ 'nema', 'venkat' ] ] }
+
 
 class HashTable {
-  constructor(size = 7) {
-    this.dataMap = new Array(size);
+  constructor(size) {
+    this.size = size;
+    this.table = new Array(size);
   }
 
   _hash(key) {
-    let hash = 0;
+    let total = 0;
     for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+      total += key.charCodeAt(i);
     }
-    return hash;
+
+    return total % this.size;
   }
 
   set(key, value) {
     let index = this._hash(key);
-    if (!this.dataMap[index]) {
-      // if there is no item in the index, first create an empty array then push the key value pair
-      this.dataMap[index] = [];
+    if (this.table[index]) {
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [[key, value]];
     }
-    // if there are existing items in the index, then directly push the key value pair to the existing array
-    this.dataMap[index].push([key, value]);
   }
 
   get(key) {
     let index = this._hash(key);
-    if (this.dataMap[index]) {
-      if (this.dataMap[index][0][0] === key) {
-        return this.dataMap[index][0][1];
-      }
-      if (this.dataMap[index].length > 1) {
-        for (let i = 0; i < this.dataMap[index].length; i++) {
-          if (this.dataMap[index][i][0] === key)
-            return this.dataMap[index][i][1];
+    if (this.table[index]) {
+      const bucket = this.table[index];
+      for (let i of bucket) {
+        if (i[0] === key) {
+          return i[1];
         }
       }
     }
-    return false;
+
+    return `${key}, Not found`;
   }
 
-  keys() {
-    let allKeys = [];
-    for (let i = 0; i < this.dataMap.length; i++) {
-      if (this.dataMap[i]) {
-        for (let j = 0; j < this.dataMap[i].length; j++) {
-          allKeys.push(this.dataMap[i][j][0]);
-        }
+  remove(key) {
+    let index = this._hash(key);
+
+    if (!this.table[index]) {
+      return `${key} not found`;
+    }
+
+    const bucket = this.table[index];
+    const matchingIndex = bucket.findIndex((item) => item[0] === key);
+    if (matchingIndex !== -1) {
+      bucket.splice(matchingIndex, 1);
+      return `${key} removed successfully`;
+    } else {
+      return `${key}, Not found`;
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.table.length; i++) {
+      if (this.table[i]) {
+        console.log({ index: i, bucket: this.table[i] });
       }
     }
-    return allKeys;
   }
 }
 
-/* ----- HashTable Creation ----- */
-let myHashTable = new HashTable();
-logger(myHashTable);
+const htb = new HashTable(20);
 
-/* ----- SET method ----- */
-myHashTable.set("screws", 8000);
-myHashTable.set("bolts", 10000);
-myHashTable.set("washers", 12000);
-myHashTable.set("lumber", 5600);
+htb.set("name", "prasan");
+htb.set("age", 35);
+htb.set("nema", "venkat");
+htb.set("ega", 6);
 
-logger(myHashTable);
+console.log("name:", htb.get("name"));
+console.log("nema:", htb.get("nema"));
+console.log("enam:", htb.get("enam"));
+console.log("age:", htb.get("age"));
+console.log("hi:", htb.get("hi"));
 
-/* ----- GET method ----- */
+console.log(htb.remove("ega"));
+console.log(htb.remove("gea"));
+console.log(htb.remove("address"));
 
-// logger(myHashTable.get("screws"));
-// logger(myHashTable.get("bolts"));
-// logger(myHashTable.get("drywall"));
-
-/* ----- KEYS method ----- */
-logger(myHashTable.keys());
+htb.display();
--- DataStructures/HashTable/main.js
+++
@@ -1,6 +0,0 @@
-// Hash Table 
-// Set to store a key-value pair
-// Get to retrieve a value given its key 
-// Remove to delete a key value pair
-// Hashing function to convert a string key to a numeric index
-
