eb2554abd738646aace196fa231a9706fc2af496
prasanv <prasan.venkatb@gmail.com>
2024-05-17 20:40:34 -0700

    reshuffle

--- DataStructures/Queue/QueueArray.js
+++ DataStructures/Queue/QueueArray.js
@@ -1,4 +1,5 @@
-// Queue
+// Queue Array
+
 // The queue data structure is a sequential collection of elements that follows the principle of First In First Out (FIFO)
 // The first element inserted into the queue is first element to be removed
 // A queue of people. People enter the queue at one end (rear/tail) and leave the queue from the other end (front/ head).
@@ -7,37 +8,45 @@
 //   • Enqueue, which adds an element to the rear/tail of the collection
 //   • Dequeue, which removes an element from the front/head of the collection
 
+// **
+// QUEUE METHODS
+// enqueue(value) - O(n)
+// dequeue() - O(n)
+// isEmpty()
+// peak()
+// size()
+// printQueue
+// **
+
 class Queue {
-    constructor() {
-        this.table = [];
-    }
+  constructor() {
+    this.table = [];
+  }
 
-    enqueue(value) {
-        return !!this.table.push(value);
-    }
+  enqueue(value) {
+    return !!this.table.push(value);
+  }
 
-    dequeue() {
-        return this.table.shift();
-    }
+  dequeue() {
+    return this.table.shift();
+  }
 
-    isEmpty() {
-        return this.table.length === 0;
-    }
+  isEmpty() {
+    return this.table.length === 0;
+  }
 
-    peak() {
-        if (this.isEmpty()) {
-            return null;
-        }
-        return this.table[0];
-    }
+  peak() {
+    if (this.isEmpty()) return null;
+    return this.table[0];
+  }
 
-    size() {
-        return this.table.length;
-    }
+  size() {
+    return this.table.length;
+  }
 
-    printQueue() {
-        return this.table.toString();
-    }
+  printQueue() {
+    return this.table.toString();
+  }
 }
 
 const Q = new Queue();
---
+++ DataStructures/Queue/QueueObject.js
@@ -0,0 +1,71 @@
+// Queue Object
+
+// **
+// QUEUE METHODS
+// enqueue(value) - O(1)
+// dequeue() - O(1)
+// isEmpty()
+// peak()
+// size()
+// printQueue
+// **
+
+class Queue {
+  constructor() {
+    this.obj = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.obj[this.rear] = value;
+    this.rear++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return null;
+    const item = this.obj[this.front];
+    delete this.obj[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.front === 0 && this.rear === 0;
+  }
+
+  peak() {
+    if (this.isEmpty()) return null;
+    return this.obj[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  printQueue() {
+    return this.obj;
+  }
+}
+
+const Q = new Queue();
+
+console.log("peak: ", Q.peak());
+console.log("isEmpty: ", Q.isEmpty());
+console.log("size: ", Q.size());
+
+console.log("dequeue: ", Q.dequeue());
+
+Q.enqueue(10);
+Q.enqueue(20);
+Q.enqueue(30);
+Q.enqueue(40);
+
+console.log("dequeue: ", Q.dequeue());
+
+console.log("peak: ", Q.peak());
+console.log("isEmpty: ", Q.isEmpty());
+console.log("size: ", Q.size());
+
+console.log("printQueue: ", Q.printQueue());
+console.log(Q);
